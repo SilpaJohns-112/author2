c722f0f552821ec1646fc876f1a6415261e0f4d7
Silpa Johns <CS5979@cloudium.local>
2021-09-17 10:27:29 +0530

    models

--- myApp.js
+++ myApp.js
@@ -1,24 +1,23 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI ,{ useNewUrlParser: true, UseUnifiedTopology: true});
 
 
-
-var Schema=mongoose.Schema;
- var personSchema = new Schema({
-  name: { type: String, required: true },
-  age: Number,
-  favoriteFoods: [String]
+var schema = mongoose.Schema;
+var Person = new mongoose.Schema ({
+name: { type: String, required: true},
+age: Number,
+favouritefoods: [String],
 });
-var Person = mongoose.model("Person", personSchema);
-let silpa=new Person({
-  name: "Silpa",
-  age:23,
-  favoriteFoods:["noodles","biriyani"]
+var Person = mongoose.model('person', PersonSchema);
+let usefulProgrammer = new person({
+  name:" usefulProgrammer",
+age: 23,
+favouritefoods: ["cake" , "ice cream" ]
 
 });
 
-
+let Person;
 
 const Person = mongoose.model("Silpa", personSchema);
 
